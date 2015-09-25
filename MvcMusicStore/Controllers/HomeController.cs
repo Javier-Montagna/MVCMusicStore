845f6fd53fd6f0f31ab8a4092bd22a2573bb820c
Javier Adrian Montagna <javiermontagna@gmail.com>
2015-09-25 17:15:28 -0300

    Added store controller

--- MvcMusicStore/Controllers/HomeController.cs
+++ MvcMusicStore/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace MvcMusicStore.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Tienda musical para entendidos.";
 
             return View();
         }
